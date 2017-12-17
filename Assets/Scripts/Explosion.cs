826f273e59108d01f140ac27392cbd0438320196
katiektara <mskater@bk.ru>
2017-12-18 01:26:41 +0300

    the new one

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+
+    public Transform explosion;
+
+    private void OnTriggerEnter(Collider hit)
+    {
+        Instantiate(explosion, transform.position, transform.rotation);
+        if (hit.gameObject.tag == "Player")
+        {
+            
+            Instantiate(explosion, gameObject.transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+       
+    }
+}
